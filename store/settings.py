af8f2e31d2f19d88bad4deba73308f7915ec2646
Max Zanchenko <zamax77@mail.ru>
2023-04-19 16:24:51 +0300

    postgresql

--- store/settings.py
+++ store/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "store_db",
+        "USER": "postgres",
+        "PASSWORD": "zamax",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
